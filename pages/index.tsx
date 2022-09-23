325593d3efaccc26c5250ac583d2686221d6cdac
luigimqf <luigi.jackson@gmail.com>
2022-09-23 17:48:55 -0300

    💄 style changes and custom font

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,14 @@
 import type { NextPage } from 'next'
 import React from 'react'
 import styles from '../styles/Home.module.css'
+import MainBanner from '../components/About/MainBanner'
 
 const Home: NextPage = () => {
 
   return (
     <section className={styles.wrapper}>
 
-      <div className={styles.main_title_container}  >
-        <div className={styles.main_title}>
-          <span className={styles.no_responsivity}>Site sem responsividade</span>
-          <h1>Meu PDI para Data Fetching c:</h1>
-          <p></p>
-        </div>
-      <div className="main_image">
-
-      </div>
-      </div>
+      <MainBanner/>
     </section>
   )
 }
